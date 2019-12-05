c8dcc54680248be841801a69fc6a7719470dfae6
Lucas Greenwell <lucasgreenwell15@gmail.com>
2019-12-05 11:35:56 -0600

    component is almost done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,10 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get("https://api.github.com/users/lucasgreenwell")
+  .then(response => {
+    console.log(response);
+  })
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -27,7 +31,8 @@
 const followersArray = [];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
-          Using DOM methods and properties, create a component that will return the following DOM element:
+          Using DOM methods and properties, create a component that will return the 
+          following DOM element:
 
 <div class="card">
   <img src={image url of user} />
@@ -46,10 +51,42 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+// function makeCard (obj){
+//   //create elements
+//   const card = document.createElement('div');
+//   const img = document.createElement('img');
+//   const cardInfo = document.createElement('div');
+//   const name = document.createElement('h3');
+//   const username = document.createElement('p');
+//   const location = document.createElement('p');
+//   const profile = document.createElement('p');
+//   const link = document.createElement('a');
+//   const followers = document.createElement('p');
+//   const following = document.createElement('p');
+//   const bio = document.createElement('p');
+
+
+//   //add classes
+//   card.classList.add('card');
+//   cardInfo.classList.add('card-info');
+//   name.classList.add('name')
+//   username.classList.add('username');
+
+//   // set up structure
+//   const cards = document.querySelector('.cards');
+//   cards.append(card);
+//   card.append(img, cardInfo);
+//   cardInfo.append(name, username, location, profile, followers, following, bio);
+//   profile.append(link);
+
+//   //add info to elements
+//   // name.textContent = ;
+//   // username.textContent = ;
+//   // location.textContent = ;
+//   // profile.textContent = ;
+//   // followers.textContent = ;
+//   // following.textContent = ;
+//   // bio.textContent = ;
+//   // img.setAttribute.src = ;
+//   // link.setAttribute.href = ;
+// }
